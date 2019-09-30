a704f7b7d6346f4c2c8a1f48fa4b3803f7e06af4
pamcuellas <pamcuellas@gmail.com>
2019-09-30 19:38:00 -0400

    Charts done.

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,32 @@
 /*jshint esversion: 6 */  
 
 // Global variables
-names = [];
-metadata = []; 
-samples = [];
+let names         = [];
+let metadata      = []; 
+let samples       = [];
+let subjectIdData = [];
+let otu_ids       = [];
+let all_otu_ids   = [];
+let values        = [];
+let allValues     = [];
+let tmpHover      = [];
+let bubbleHoverText= [];
+let hovertext     = [];
+let labels        = [];
+let currSubjectId = "963";
 
+// Sort array in desc order.
+let sortArrayDesc = (a,b) => b-a;
+
+// Split the dataset. 
 d3.json("./static/data/samples.json")
     .then( data =>  {
         names = data.names; 
-        names.unshift("Choose an ID...");
+        names.unshift("Choose other ID...");
         metadata = data.metadata;
         samples  = data.samples; 
 
-        //*********** Populate dropdown  
+        // Grab dropdown element  
         let dropdown = d3.select("#selDataset");
 
         // Clear drop-down content 
@@ -24,104 +38,159 @@ d3.json("./static/data/samples.json")
         .data(names)
         .enter()
         .append("option")  
-            .text( d => d);	
+            .text( d => d);
+        
+        filterData ( );
+        init();
 });
 
-
+// Event for select Subject Id
 d3.select("#selDataset").on('change', () => {
 
-    if (parseInt(d3.event.target.value)) { 
-        /* PROCESS TO PLOT BAR CHART */    
-        const subjectIdData = samples.find( obj => obj.id === d3.event.target.value );
-        const otu_ids       = subjectIdData.otu_ids.slice(0,10).reverse();
-        const all_otu_ids   = subjectIdData.otu_ids.reverse();
-        const values        = subjectIdData.sample_values.slice(0,10).reverse();
-        const allValues     = subjectIdData.sample_values.reverse();
-        const tmpHover      = subjectIdData.otu_labels.slice(0,10).reverse();
-        const all_otu_labels= subjectIdData.otu_labels.reverse();
-        const hovertext = tmpHover.map( text => text.replace(/;/g, " ") );
-        let labels = otu_ids.map( val => 'OTU ' + val );
-        plotBar(values, labels, hovertext);
+    currSubjectId = d3.event.target.value;
+    if (parseInt( currSubjectId )) { 
 
-        plotBubble(all_otu_ids, allValues, all_otu_labels);
+      filterData();
 
-        // UPDATE THE DEMOGRAPHIC INFO 
-        const tmpDemoInfo = metadata.find( obj => {
-            return obj.id === parseInt(d3.event.target.value);
-        });
-        demoInfo = Object.entries( tmpDemoInfo ).map(  obj =>  obj[0] + ": " + obj[1] );
-        var item = d3.select('#sample-metadata ul')
-            .selectAll('li')
-            .data(demoInfo);
-        // Enter
-        item
-            .enter()
-            .append('li')
-            .attr('class', 'li')
-            .text( d => d);
-        // Update
-        item
-            .text( d => d );
-        // Exit
-        item.exit().remove();
+      updBarChart();
+
+      updBubbleChart();
+
+      updDemoInfo();
 
     }
 });
 
 
-let plotBar = ( values, labels, hovertext ) => {
+// Update  Bar Chart;
+let updBarChart = ( ) => {
+  var layoutBar = {
+    title: 'Subject ID ' + currSubjectId
+  };
+  Plotly.relayout("bar", layoutBar);
+  Plotly.restyle("bar", "x", [values]);
+  Plotly.restyle("bar", "y", [labels]);
+  Plotly.restyle("bar", "hovertext", [hovertext]);
+};
 
-    var trace1 = {
-        x: values,
-        y: labels,
-        hovertext: hovertext,
-        orientation: 'h',
-        marker: {
-          color: 'rgba(55,128,191,0.6)',
-          width: 0.1
-        },
-        type: 'bar'
-      };
+// Update Bubble Chart
+let updBubbleChart = ( ) => {
 
-      var data = [trace1];
+  let marker =  {
+    size: allValues,
+    color: all_otu_ids
+  }; 
 
-      var layout = {
-        title: 'Title'
-      };
+  Plotly.restyle("bubble", "x", [all_otu_ids]);
+  Plotly.restyle("bubble", "y", [allValues]);
+  Plotly.restyle("bubble", "hovertext", [bubbleHoverText]);
+  Plotly.restyle("bubble", "marker", [marker]);
+};
 
-      Plotly.newPlot('bar', data, layout);
+// Update Demographic Info
+let updDemoInfo = () => {
+  const tmpDemoInfo = metadata.find( obj => {
+    return obj.id === parseInt(currSubjectId);
+  });
+  demoInfo = Object.entries( tmpDemoInfo ).map(  obj =>  obj[0] + ": " + obj[1] );      
+  var item = d3.select('#sample-metadata ul')
+      .selectAll('li')
+      .data(demoInfo);
+  // Enter
+  item
+      .enter()
+      .append('li')
+      .attr('class', 'li')
+      .text( d => d);
+  // Update
+  item
+      .text( d => d );
+  // Exit
+  item.exit().remove();
+};
+
+// Create the charts
+let init = () => {
+ 
+  // Plot Bar Chart
+  // ******************************  
+  var barTrace = {
+    x: values,
+    y: labels,
+    hovertext: hovertext,
+    orientation: 'h',
+    marker: {
+      color: 'rgba(55,128,191,0.6)',
+      width: 0.1
+    },
+    type: 'bar'
+  };
+
+  var barData = [barTrace];
+  var barLayout = {
+    title: 'Subject ID ' + currSubjectId
+  };
+
+  Plotly.newPlot('bar', barData, barLayout);
+
+  // Plot Bubble Chart 
+  // *************************  
+  var bubbleTrace = {
+    x: all_otu_ids,
+    y: allValues,
+    hovertext: bubbleHoverText,
+    mode: 'markers',
+    marker: {
+      size: allValues,
+      color: all_otu_ids
+    }
+  };
+  
+  bubbleData = [bubbleTrace];
+  
+  bubbleLayout = {
+    xaxis: { title: "OTU ID"},
+    yaxis: { title: "Samples" }
+  };
+
+  Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+  updDemoInfo();
+
 };
 
 
-let plotBubble = ( allValues, labels,  hovertext ) => {
+// Filter Data according with the subject Id selected.
+let  filterData = ( ) => {
+  
+  subjectIdData = samples.find( obj => obj.id === currSubjectId);
 
-    var trace1 = {
-        x: allValues,
-        y: labels,
-        hovertext: hovertext,
-        mode: 'markers',
-        marker: {
-          size: labels,
-          color: allValues
-        }
-      };
-      
-      var data = [trace1];
-      
-      var layout = {
-        xaxis: { title: "OTU ID"},
-        yaxis: { title: "Samples" }
-      };
+  // the values for the bar chart
+  values        = subjectIdData.sample_values.slice();
+  values.sort(sortArrayDesc);
+  values = values.slice(0, 10).reverse();
 
-      Plotly.newPlot('bubble', data, layout);
-};
+  // the labels for the bar chart 
+  otu_ids       = subjectIdData.otu_ids.slice(0,10);
+  otu_ids.reverse();
+  labels          = otu_ids.map( val => 'OTU ' + val );
 
+  // For the x values Bubble and the marker colors
+  all_otu_ids   = subjectIdData.otu_ids.slice();
+  all_otu_ids.reverse();
+  
+  // For the y values Bubble and marker size 
+  allValues     = subjectIdData.sample_values.slice();
+  allValues.reverse();
 
+  // the hovertext for the chart
+  tmpHover      = subjectIdData.otu_labels.slice(0,10);
+  tmpHover.reverse();
+  hovertext     = tmpHover.map( text => text.replace(/;/g, " ") );
 
-// Plotly.restyle(graphDiv, update [, traceIndices])
-// graphDiv
-// DOM node or string id of a DOM node
-// update
-// object, see below for examples 
-// (defaults to {})
-// traceIndices
+  // Hover Text for Bubble chart (text values)
+  bubbleHoverText= subjectIdData.otu_labels.slice(); 
+  bubbleHoverText.reverse();
+  
+}
+
